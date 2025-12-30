52830f47d1edd4780509d629da4cd17ab26061ed
ludik <bakhanetssimon@gmail.com>
2025-12-31 02:03:06 +0700

    Initial Commit

---
+++ BelShop Smart/BelShop_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelShop_SmartApp.swift
+//  BelShop Smart
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BelShop_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BelShop SmartTests/BelShop_SmartTests.swift
@@ -0,0 +1,16 @@
+//
+//  BelShop_SmartTests.swift
+//  BelShop SmartTests
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import Testing
+
+struct BelShop_SmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
